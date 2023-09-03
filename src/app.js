a9e2e657261261f3e8878fc467abb807f0d12501
felicitasamanta <samantafelicita@gmail.com>
2023-09-03 20:37:57 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { icons } from "./assets/icons/icons.js";
+import * as selectors from "./ui/common/selectors.js";
+
+selectors.menu_weather_icon.innerHTML = icons.weather;
+selectors.menu_cities_icon.innerHTML = icons.cities;
+selectors.menu_map_icon.innerHTML = icons.map;
+selectors.menu_settings_icon.innerHTML = icons.settings;
+selectors.real_feel.innerHTML = icons.real_feel;
+selectors.wind.innerHTML = icons.wind;
+selectors.chance_of_rain.innerHTML = icons.chance_of_rain;
+selectors.uv_index.innerHTML = icons.uv_index;
---
+++ src/ui/common/selectors.js
@@ -0,0 +1,23 @@
+// icons
+const menu_weather_icon = document.querySelector(".menu_weather_icon");
+const menu_cities_icon = document.querySelector(".menu_cities_icon");
+const menu_map_icon = document.querySelector(".menu_map_icon");
+const menu_settings_icon = document.querySelector(".menu_settings_icon");
+
+const real_feel = document.querySelector(".real_feel");
+const chance_of_rain = document.querySelector(".chance_of_rain");
+const wind = document.querySelector(".wind");
+const uv_index = document.querySelector(".uv_index");
+
+//
+
+export {
+  menu_weather_icon,
+  menu_cities_icon,
+  menu_map_icon,
+  menu_settings_icon,
+  real_feel,
+  chance_of_rain,
+  wind,
+  uv_index,
+};
